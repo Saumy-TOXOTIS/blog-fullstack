9cf69881d4ef66a427d6ca037de6af0c210469f1
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-06-09 01:45:58 +0530

    fix: Implement vite-plugin-single-spa for routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import rewriteAll from 'vite-plugin-rewrite-all';
+import singleSpa from 'vite-plugin-single-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), rewriteAll()],
+  plugins: [react(), tailwindcss(), rewriteAll(),singleSpa({
+      type: 'root',
+      imo: false, // Important for this use case
+    })],
   server: {
     host: '0.0.0.0'
   }
