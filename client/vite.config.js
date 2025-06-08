7ccc14ea9a5823e3e6fe246c0e00f0ed425e1c93
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-06-08 13:18:34 +0530

    fix: Implement standard PostCSS configuration for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
-// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Allow external connections
+    allowedHosts: [
+      '22e5-2401-4900-5dba-32ff-4599-bd1f-9dd1-e92d.ngrok-free.app'
+    ]
+  }
 })
