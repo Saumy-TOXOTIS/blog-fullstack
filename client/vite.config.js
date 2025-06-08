cfdaf19f435ba58e3bdb34a421b5c971b38a30e5
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-06-08 13:09:10 +0530

    chore: Simplify Vite config for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // We REMOVED tailwindcss() from here.
-  plugins: [react()], 
-  
-  // The server config can remain for your local ngrok setup.
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      '22e5-2401-4900-5dba-32ff-4599-bd1f-9dd1-e92d.ngrok-free.app'
-    ]
-  }
+  plugins: [react()],
 })
